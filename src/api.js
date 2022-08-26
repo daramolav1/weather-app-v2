812a37e463ebec8b2b344043e245f0c9577ec042
Victor <victorddaramola@gmail.com>
2022-08-25 22:02:43 -0400

    Functions to make call to API

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "40c668ed8dmshb0b4d43342eba3bp167e94jsnda142bd529b7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "4bc650ab1b8cd710e9d05d73e17442e6";
